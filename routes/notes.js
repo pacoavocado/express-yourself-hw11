a74a511f7e4ca89f6cdb8acc90b94084bdcd33e4
Micah Conn <mcoxconn@gmail.com>
2021-09-01 16:45:40 -0500

    getting there?

---
+++ routes/notes.js
@@ -0,0 +1,57 @@
+const notes = require('express').Router();
+
+
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+notes.get('/:db_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.note_id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID');
+      });
+  });
+  
+
+// DELETE Route for a specific note
+notes.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all notes except the one with the ID provided in the URL
+        const result = json.filter((note) => note.note_id !== noteId);  
+        
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`note added successfully`);
+    } else {
+      res.error('Error in adding tip');
+    }
+  });
+  
+  module.exports = notes;
