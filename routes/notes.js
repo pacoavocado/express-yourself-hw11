57b0b42f11112a340b67c66e6f1f3d9bf39c35fb
Micah Conn <mcoxconn@gmail.com>
2021-09-06 12:22:54 -0500

    cannot for the life of me get the save button to populate

--- routes/notes.js
+++ routes/notes.js
@@ -1,66 +1,64 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readFromFile, readAndAppend, readFromFile } = require('../helpers/fsUtils');
-const path = require('path');
-
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+// const path = require('path');
+const noteDb = require('../db/db.json');
 // const app = express();
 // const noteForm = document.querySelector('.icons');
 
 notes.get('/', (req, res) =>
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+  readFromFile('./db/db.json').then((noteDb) => res.json(noteDb))
 );
 
 // GET Route for a specific note
-notes.get('/api/notes/:id', (req, res) => {
-  const noteId = req.params.note_id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      const result = json.filter((notes) => notes.notes_id === noteId);
-      return result.length > 0
-        ? res.json(result)
-        : res.json('no notes');
-    });
-});
-
-
-// DELETE Route for a specific tip
-notes.delete('/api/notes/:id', (req, res) => {
-  const noteId = req.params.id;
-  readFromFile('./db/db.json')
-    .then((notes) => JSON.parse(notes))
-    .then((json) => {
-      // Make a new array of all notes except the one with the ID provided in the URL
-      const result = allNotes.filter((notes) => notes.id !== noteId);
-
-      // Save that array to the filesystem
-      writeToFile('./db/db.json', result);
-
-      // Respond to the DELETE request
-      res.json(`Item note has been deleted`);
-    });
-});
+// notes.get('/:id', (req, res) => {
+//   const noteId = req.params.note_id;
+//   readFromFile('./db/db.json')
+//     .then((noteDb) => JSON.parse(noteDb))
+//     .then((json) => {
+//       const noteDb = json.filter((notes) => notes.notes_id === noteId);
+//       return noteDb.length > 0
+//         ? res.json(noteDb)
+//         : res.json('no notes');
+//     });
+// });
 
 
 // POST Route for a new note
-notes.post('/api/notes/:id', (req, res) => {
-  console.log(req.body);
+notes.post('/', (req, res) => {
+ 
 
-  const { noteTitle, noteText } = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
     const newNote = {
       noteTitle,
       noteText,
-      note_id: uuidv4(),
+      id: uuidv4(),
     };
-
+    noteDb.push(newNote);
     readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully`);
-  } else {
-    res.error('Error in adding tip');
-  }
+    res.json(newNote);
+  } 
 });
+
+// DELETE Route for a specific note
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params;
+  readFromFile('./db/db.json')
+    .then((notes) => JSON.parse(notes))
+    .then((json) => {
+      // Make a new array of all notes except the one with the noteI provided in the URL
+      let noteDb = noteDb.filter((notes) => notes.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', noteDb);
+
+      // Respond to the DELETE request
+      res.json(noteDb);
+    });
+});
+
 
   
   module.exports = notes;
